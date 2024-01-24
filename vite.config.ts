235d0ccd4c0e3690a4817fd7d73276f213da90dc
ilyatitovich <ilyatitovich@gmail.com>
2024-01-24 11:09:31 +0100

    pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,60 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import type { VitePWAOptions } from "vite-plugin-pwa";
+import { VitePWA } from "vite-plugin-pwa";
 
+const pwaOptions: Partial<VitePWAOptions> = {
+    registerType: "autoUpdate",
+    mode: "development",
+    base: "/",
+    includeAssets: ["favicon.ico", "apple-touch-icon.png", "mask-icon.svg"],
+    manifest: {
+        name: "Memento",
+        short_name: "Memento",
+        description:
+            "An app for memorizing information using spaced repetition techniques",
+        theme_color: "#ffffff",
+        icons: [
+            {
+                src: "pwa-64x64.png",
+                sizes: "64x64",
+                type: "image/png",
+            },
+            {
+                src: "pwa-192x192.png",
+                sizes: "192x192",
+                type: "image/png",
+            },
+            {
+                src: "/pwa-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+            },
+            {
+                src: "pwa-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "any",
+            },
+            {
+                src: "pwa-512x512.png",
+                sizes: "512x512",
+                type: "image/png",
+                purpose: "maskable",
+            },
+        ],
+    },
+    devOptions: {
+        enabled: true,
+    },
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-        scss: {},
+    plugins: [react(), VitePWA(pwaOptions)],
+    css: {
+        preprocessorOptions: {
+            scss: {},
+        },
     },
-},
-})
+});
