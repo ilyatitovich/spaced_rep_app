80c29f219d0de0341c75304cc956c38632f35a75
Ilya Titov <ilyatitovdev@gmail.com>
2025-05-20 12:34:54 +0200

    Add tailwind

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
-import "./index.scss";
+import './index.scss'
+import './index.css'
 
-import React from "react";
-import ReactDOM from "react-dom/client";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-import App from "./App";
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
