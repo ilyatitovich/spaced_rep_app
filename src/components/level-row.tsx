20e63e5626454316b77cb1cc10d9f604944a6eaf
Ilya Titov <ilyatitovdev@gmail.com>
2025-11-15 11:49:06 +0100

    Fix text size

--- src/components/level-row.tsx
+++ src/components/level-row.tsx
@@ -10,7 +10,7 @@ export default function LevelRow({ levelId, cardsNumber }: LevelRowProps) {
   let leftContent = (
     <>
       <span className={`w-2 h-2 rounded-full bg-lvl-${levelId}`}></span>
-      <span className="flex flex-col text-black text-lg">
+      <span className="flex flex-col text-black">
         <span>{`Level ${levelId}`}</span>
         <span className="text-gray-500 text-sm">
           {levelId === 1
@@ -38,7 +38,7 @@ export default function LevelRow({ levelId, cardsNumber }: LevelRowProps) {
   return (
     <li className="py-3.5 border-b border-gray-200 last:border-b-0">
       <Link to={`${levelId}`} className="flex justify-between items-center">
-        <span className="flex items-center gap-3">{leftContent}</span>
+        <span className="flex items-center gap-3 text-lg">{leftContent}</span>
         <span className="flex items-center gap-3 text-gray-500">
           <span className="text-lg">{`${cardsNumber} cards`}</span>
           <ChevronRight className="text-sm" />
