21a382af285c2966336e4bed0c2b1faaf221548d
Ilya Titov <ilyatitovdev@gmail.com>
2025-11-23 10:36:09 +0100

    Remove height calculations

--- src/pages/root.tsx
+++ src/pages/root.tsx
@@ -1,18 +1,5 @@
-import { useEffect } from 'react'
 import { Outlet } from 'react-router'
 
 export default function Root() {
-  useEffect(() => {
-    function setVh() {
-      const vh = window.innerHeight * 0.01
-      document.documentElement.style.setProperty('--vh', `${vh}px`)
-    }
-
-    setVh()
-
-    window.addEventListener('resize', setVh)
-
-    return () => window.removeEventListener('resize', setVh)
-  }, [])
   return <Outlet />
 }
