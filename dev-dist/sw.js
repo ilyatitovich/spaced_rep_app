764b2f6b92d5c443400a61afde36dd3a4a650c4b
Ilya Titov <ilyatitovdev@gmail.com>
2025-11-24 11:52:26 +0100

    Fix ts config

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.veo6hethqvg"
+    "revision": "0.e4e61mfaco8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
