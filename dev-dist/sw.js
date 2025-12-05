be3e8f9d90e04aecf6f627bb3a393a9e9dff4a76
Ilya Titov <ilyatitovdev@gmail.com>
2025-12-05 15:46:31 +0100

    Change sw

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-86c9b217'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ropgkfk2n5o"
+    "revision": "0.6bfhnititp"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
