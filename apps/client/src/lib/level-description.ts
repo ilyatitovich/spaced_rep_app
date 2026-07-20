3806da65cb35d9dbc2ce6ff05b4b82a8c37166eb
Ilya Titov <ilyatitovdev@gmail.com>
2026-07-20 10:28:42 +0200

    Update level description

--- apps/client/src/lib/level-description.ts
+++ apps/client/src/lib/level-description.ts
@@ -1,8 +1,7 @@
 const levelDescription: Record<string, string> = {
   0: 'Cards with only one side are served as drafts. You can also move cards to drafts if you are not ready to review them.',
   1: 'New cards are added to level 1. When you review a card, and get it right, it moves up one level. When you review a card, and get it wrong, it moved back to level 1.',
-  2: `When you review a card, and get it right, it moves up one level. When you review a card, and get it wrong, it moved back to level 1.
-  It might seems as if cards you got wrong stayed at level 2. That is only because the test repeats all level 1 card until you get the right.`,
+  2: `When you review a card, and get it right, it moves up one level. When you review a card, and get it wrong, it moved back to level 1.`,
   general:
     'When you review a card, and get it right, it moves up one level. When you review a card, and get it wrong, it moved back to level 1.',
   7: 'When you review a card, and get it right, it moves to finished cards. When you review a card, and get it wrong, it moved back to level 1.',
