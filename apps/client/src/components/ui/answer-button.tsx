bf1ec7897f06e0430a6ffff1c739ef163b6b3b7d
Ilya Titov <ilyatitovdev@gmail.com>
2026-09-12 14:04:34 +0200

    Update AnswerButton component to use bg-linear-to-br class for background gradient

--- apps/client/src/components/ui/answer-button.tsx
+++ apps/client/src/components/ui/answer-button.tsx
@@ -10,7 +10,7 @@ export default function AnswerButton({
   return (
     <button
       onClick={() => onAnswer(isCorrect)}
-      className={`w-full font-black text-primary-foreground bg-gradient-to-br ${isCorrect ? 'from-success to-success-hover' : 'from-danger to-danger-hover'} py-4 px-10 rounded-xl shadow-lg active:scale-95 transition-all duration-200`}
+      className={`w-full font-black text-primary-foreground bg-linear-to-br ${isCorrect ? 'from-success to-success-hover' : 'from-danger to-danger-hover'} py-4 px-10 rounded-xl shadow-lg active:scale-95 transition-all duration-200`}
     >
       {isCorrect ? 'Correct' : 'Wrong'}
     </button>
