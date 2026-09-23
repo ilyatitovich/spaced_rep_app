0e62fabfa166ad60279ae02d43bb196c2c386860
Ilya Titov <ilyatitovdev@gmail.com>
2026-09-23 11:08:43 +0200

    Add ephemeral storage cleanup on pagehide

--- apps/browser-extension/entrypoints/sidepanel/main.tsx
+++ apps/browser-extension/entrypoints/sidepanel/main.tsx
@@ -1,6 +1,12 @@
 import { createRoot } from 'react-dom/client'
+import { clearEphemeralStorage } from '@ext/lib/chrome-store'
 import App from './App'
 import '@ext/styles.css'
+
+window.addEventListener('pagehide', event => {
+  if (event.persisted) return
+  void clearEphemeralStorage()
+})
 
 const container = document.getElementById('app')
 
