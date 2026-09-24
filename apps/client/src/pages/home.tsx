3672880384be7ca01b1c7813214ae6ab2c5f292a
Ilya Titov <ilyatitovdev@gmail.com>
2026-09-24 11:50:08 +0200

    Update padding in ArchivedTopicsScreen and HomePage: Increased bottom padding for list elements in selection mode to enhance layout consistency and responsiveness.

--- apps/client/src/pages/home.tsx
+++ apps/client/src/pages/home.tsx
@@ -151,7 +151,7 @@ export default function HomePage() {
             <ul
               className={`h-full px-4 pt-0 overflow-y-auto ${
                 isSelectionMode
-                  ? 'pb-[calc(8rem+env(safe-area-inset-bottom,0px))]'
+                  ? 'pb-[calc(10rem+env(safe-area-inset-bottom,0px))]'
                   : 'pb-[calc(6.5rem+env(safe-area-inset-bottom,0px))]'
               }`}
             >
